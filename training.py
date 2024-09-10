6ab713b3feec0b24dd74f7fefe6ae0790ad8c5b3
Vivek Boominathan <vivek.booms@gmail.com>
2024-09-10 17:47:58 -0500

    MPS backend added

--- training.py
+++ training.py
@@ -1,5 +1,5 @@
-'''Implements a generic training loop.
-'''
+"""Implements a generic training loop.
+"""
 
 import torch
 import utils
@@ -11,28 +11,49 @@ import os
 import shutil
 
 
-def train(model, train_dataloader, epochs, lr, steps_til_summary, epochs_til_checkpoint, model_dir, loss_fn,
-          summary_fn, val_dataloader=None, double_precision=False, clip_grad=False, use_lbfgs=False, loss_schedules=None):
+def train(
+    model,
+    train_dataloader,
+    epochs,
+    lr,
+    steps_til_summary,
+    epochs_til_checkpoint,
+    model_dir,
+    loss_fn,
+    summary_fn,
+    val_dataloader=None,
+    double_precision=False,
+    clip_grad=False,
+    use_lbfgs=False,
+    loss_schedules=None,
+    device="cpu",
+):
 
     optim = torch.optim.Adam(lr=lr, params=model.parameters())
 
-    # copy settings from Raissi et al. (2019) and here 
+    # copy settings from Raissi et al. (2019) and here
     # https://github.com/maziarraissi/PINNs
     if use_lbfgs:
-        optim = torch.optim.LBFGS(lr=lr, params=model.parameters(), max_iter=50000, max_eval=50000,
-                                  history_size=50, line_search_fn='strong_wolfe')
+        optim = torch.optim.LBFGS(
+            lr=lr,
+            params=model.parameters(),
+            max_iter=50000,
+            max_eval=50000,
+            history_size=50,
+            line_search_fn="strong_wolfe",
+        )
 
     if os.path.exists(model_dir):
-        val = input("The model directory %s exists. Overwrite? (y/n)"%model_dir)
-        if val == 'y':
+        val = input("The model directory %s exists. Overwrite? (y/n)" % model_dir)
+        if val == "y":
             shutil.rmtree(model_dir)
 
     os.makedirs(model_dir)
 
-    summaries_dir = os.path.join(model_dir, 'summaries')
+    summaries_dir = os.path.join(model_dir, "summaries")
     utils.cond_mkdir(summaries_dir)
 
-    checkpoints_dir = os.path.join(model_dir, 'checkpoints')
+    checkpoints_dir = os.path.join(model_dir, "checkpoints")
     utils.cond_mkdir(checkpoints_dir)
 
     writer = SummaryWriter(summaries_dir)
@@ -42,37 +63,37 @@ def train(model, train_dataloader, epochs, lr, steps_til_summary, epochs_til_che
         train_losses = []
         for epoch in range(epochs):
             if not epoch % epochs_til_checkpoint and epoch:
-                torch.save(model.state_dict(),
-                           os.path.join(checkpoints_dir, 'model_epoch_%04d.pth' % epoch))
-                np.savetxt(os.path.join(checkpoints_dir, 'train_losses_epoch_%04d.txt' % epoch),
-                           np.array(train_losses))
+                torch.save(model.state_dict(), os.path.join(checkpoints_dir, "model_epoch_%04d.pth" % epoch))
+                np.savetxt(os.path.join(checkpoints_dir, "train_losses_epoch_%04d.txt" % epoch), np.array(train_losses))
 
             for step, (model_input, gt) in enumerate(train_dataloader):
                 start_time = time.time()
-            
-                model_input = {key: value.cuda() for key, value in model_input.items()}
-                gt = {key: value.cuda() for key, value in gt.items()}
+
+                model_input = {key: value.to(device) for key, value in model_input.items()}
+                gt = {key: value.to(device) for key, value in gt.items()}
 
                 if double_precision:
                     model_input = {key: value.double() for key, value in model_input.items()}
                     gt = {key: value.double() for key, value in gt.items()}
 
                 if use_lbfgs:
+
                     def closure():
                         optim.zero_grad()
                         model_output = model(model_input)
                         losses = loss_fn(model_output, gt)
-                        train_loss = 0.
+                        train_loss = 0.0
                         for loss_name, loss in losses.items():
-                            train_loss += loss.mean() 
+                            train_loss += loss.mean()
                         train_loss.backward()
                         return train_loss
+
                     optim.step(closure)
 
                 model_output = model(model_input)
                 losses = loss_fn(model_output, gt)
 
-                train_loss = 0.
+                train_loss = 0.0
                 for loss_name, loss in losses.items():
                     single_loss = loss.mean()
 
@@ -87,9 +108,8 @@ def train(model, train_dataloader, epochs, lr, steps_til_summary, epochs_til_che
                 writer.add_scalar("total_train_loss", train_loss, total_steps)
 
                 if not total_steps % steps_til_summary:
-                    torch.save(model.state_dict(),
-                               os.path.join(checkpoints_dir, 'model_current.pth'))
-                    summary_fn(model, model_input, gt, model_output, writer, total_steps)
+                    torch.save(model.state_dict(), os.path.join(checkpoints_dir, "model_current.pth"))
+                    summary_fn(model, model_input, gt, model_output, writer, total_steps, device=device)
 
                 if not use_lbfgs:
                     optim.zero_grad()
@@ -97,7 +117,7 @@ def train(model, train_dataloader, epochs, lr, steps_til_summary, epochs_til_che
 
                     if clip_grad:
                         if isinstance(clip_grad, bool):
-                            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.)
+                            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
                         else:
                             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=clip_grad)
 
@@ -106,14 +126,17 @@ def train(model, train_dataloader, epochs, lr, steps_til_summary, epochs_til_che
                 pbar.update(1)
 
                 if not total_steps % steps_til_summary:
-                    tqdm.write("Epoch %d, Total loss %0.6f, iteration time %0.6f" % (epoch, train_loss, time.time() - start_time))
+                    tqdm.write(
+                        "Epoch %d, Total loss %0.6f, iteration time %0.6f"
+                        % (epoch, train_loss, time.time() - start_time)
+                    )
 
                     if val_dataloader is not None:
                         print("Running validation set...")
                         model.eval()
                         with torch.no_grad():
                             val_losses = []
-                            for (model_input, gt) in val_dataloader:
+                            for model_input, gt in val_dataloader:
                                 model_output = model(model_input)
                                 val_loss = loss_fn(model_output, gt)
                                 val_losses.append(val_loss)
@@ -123,17 +146,15 @@ def train(model, train_dataloader, epochs, lr, steps_til_summary, epochs_til_che
 
                 total_steps += 1
 
-        torch.save(model.state_dict(),
-                   os.path.join(checkpoints_dir, 'model_final.pth'))
-        np.savetxt(os.path.join(checkpoints_dir, 'train_losses_final.txt'),
-                   np.array(train_losses))
+        torch.save(model.state_dict(), os.path.join(checkpoints_dir, "model_final.pth"))
+        np.savetxt(os.path.join(checkpoints_dir, "train_losses_final.txt"), np.array(train_losses))
 
 
-class LinearDecaySchedule():
+class LinearDecaySchedule:
     def __init__(self, start_val, final_val, num_steps):
         self.start_val = start_val
         self.final_val = final_val
         self.num_steps = num_steps
 
     def __call__(self, iter):
-        return self.start_val + (self.final_val - self.start_val) * min(iter / self.num_steps, 1.)
+        return self.start_val + (self.final_val - self.start_val) * min(iter / self.num_steps, 1.0)
